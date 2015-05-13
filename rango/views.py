a0c0a0ed73c57a6d34e806b597d3fef7edc3f569
Zachary R Smith <ZacharySmith4989@gmail.com>
2015-05-13 01:01:22 +0000

    Finish ch5

--- rango/views.py
+++ rango/views.py
@@ -2,8 +2,7 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def about(request):
-    return HttpResponse("Rango says here is the about page. <br/>" +
-                        "<a href='/rango'>Main</a>")
+    return render(request, 'rango/about.html')
 
 def index(request):
     context_dict = { 'boldmessage': "I am bold font from the context" }
