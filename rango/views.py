4d05584fd2abea64862511e95c22c80043980417
Zachary R Smith <ZacharySmith4989@gmail.com>
2015-05-14 17:26:38 +0000

    Finish ch7

--- rango/views.py
+++ rango/views.py
@@ -23,6 +23,8 @@ def category(request, category_name_slug):
 
 def index(request):
     category_list = Category.objects.order_by('-likes')[:5]
-    context_dict  = { 'categories': category_list }
+    most_visited_pages = Page.objects.order_by('-views')[:5]
+    context_dict  = { 'categories': category_list,
+                      'most_visited_pages': most_visited_pages }
 
     return render(request, 'rango/index.html', context_dict)
