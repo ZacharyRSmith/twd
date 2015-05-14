acd2acb084fcb557e8a0af89342e895bd027c833
Zachary R Smith <ZacharySmith4989@gmail.com>
2015-05-14 15:38:55 +0000

    7.2

--- rango/views.py
+++ rango/views.py
@@ -1,10 +1,12 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from rango.models import Category
 
 def about(request):
     return render(request, 'rango/about.html')
 
 def index(request):
-    context_dict = { 'boldmessage': "I am bold font from the context" }
+    category_list = Category.objects.order_by('-likes')[:5]
+    context_dict  = { 'categories': category_list }
 
     return render(request, 'rango/index.html', context_dict)
