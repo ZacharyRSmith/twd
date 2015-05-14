a8927fc43eeddebaf2d918c800f692d559829c62
Zachary R Smith <ZacharySmith4989@gmail.com>
2015-05-14 20:35:04 +0000

    9.5

--- rango/views.py
+++ rango/views.py
@@ -1,4 +1,5 @@
-from django.http import HttpResponse
+from django.contrib.auth import authenticate, login
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from rango.forms import CategoryForm, PageForm, UserForm, UserProfileForm
 from rango.models import Category, Page
@@ -110,3 +111,22 @@ def register(request):
                  'rango/register.html',
                  { 'user_form': user_form, 'profile_form': profile_form,
                    'registered': registered })
+
+def user_login(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(username=username, password=password)
+
+        if user:
+            if user.is_active:
+                login(request, user)
+                return HttpResponseRedirect('/rango/')
+            else:
+                return HttpResponse("Your Rango account is disabled. Pfft!")
+        else:
+            print "Invalid login details: {0}, {1}".format(username, password)
+            return HttpResponse("Invalid login details supplied.")
+    else:
+        return render(request, 'rango/login.html', { })
